b3ab1320e8613f760abf1746e53fab6c247fe0e2
João Victor Cordeiro <jvcordeiroc1@gmail.com>
2024-05-20 17:26:18 -0300

    style: atualizar imagnes e esquema de cores

--- script.js
+++ script.js
@@ -2,16 +2,23 @@ const html = document.querySelector('html')
 const focoBt = document.querySelector('.app__card-button--foco')
 const curtoBt = document.querySelector('.app__card-button--curto')
 const longoBt = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
 
 focoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')     // Transforma o valor atribuito a string do html
+    banner.setAttribute('src', '/imagens/gengar3.jpg')
 })
 
 curtoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/jiggy.png')                          
 })
 
 longoBt.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/imagens/snorlax.png')  
 })
+
+
+
